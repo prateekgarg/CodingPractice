92a5283be630082b92b287292b1cd050f6d8e22f
Prateek <garg.prateek1@gmail.com>
2017-09-13 00:22:56 -0700

    addded problem statement and test cases

--- src/com/prateek/URLify/Main.java
+++ src/com/prateek/URLify/Main.java
@@ -4,17 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        //Problem statement:
+        //Problem statement: Convert the string given to URL format
+        //That means, all spaces convert to '%20'
         testURLify();
 
     }
 
     public static void testURLify() {
         URLify trial = new URLify();
-        String[] testCases = {"", " d  ", null};
-
-        trial.setInput("Mr John Smith    ");
-        System.out.println(trial.URLifyString());
+        String[] testCases = {"", " d  ", null, "Mr John Smith    "};
+        for (String test : testCases) {
+            trial.setInput(test);
+            System.out.println(trial.URLifyString());
+        }
     }
 
 }
