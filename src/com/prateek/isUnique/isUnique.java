e9cbddab6696a42610ea6023a87274a9e071c8ca
Prateek <garg.prateek1@gmail.com>
2017-09-15 00:08:49 -0700

    Added an O(1) space solution as well

--- src/com/prateek/isUnique/isUnique.java
+++ src/com/prateek/isUnique/isUnique.java
@@ -11,6 +11,11 @@ public class isUnique {
         System.out.println("Call number 3: " + (solutionOne(""))); // should return false
         System.out.println("Call number 4: " + (solutionOne(null))); // should return false
 
+        System.out.println("Call number 1: " + (solutionTwo("abcdefgHijklmnopq12345678"))); // should return true
+        System.out.println("Call number 2: " + (solutionTwo("aAbB"))); // should return true as we are taking 256 charset
+        System.out.println("Call number 2: " + (solutionTwo("abcdefgha"))); // should return false
+        System.out.println("Call number 3: " + (solutionTwo(""))); // should return false
+        System.out.println("Call number 4: " + (solutionTwo(null))); // should return false
     }
 
     private static boolean solutionOne(String input) {
@@ -32,5 +37,26 @@ public class isUnique {
         //Reached the end of the string so there are no repetition of any charaters.
         return true;
     }
-    
+
+    static boolean solutionTwo(String input) {
+        if (input == null || input.equals("")) {
+            return false;
+        }
+        //Here we assume we are not dealing with Unicode, and we are just dealing with an ASCII character set.
+        //If we assume just lowercase alphabetical characters, we can make do with an array of size 26
+        boolean[] keeper = new boolean[256];
+
+        for (int i = 0; i < input.length(); i++) {
+            int value = input.charAt(i);
+            if (keeper[value]) {
+                //we have encountered this value before
+                return false;
+            } else {
+                keeper[value] = true;
+            }
+        }
+
+        //Got through the string without repetition
+        return true;
+    }
 }
